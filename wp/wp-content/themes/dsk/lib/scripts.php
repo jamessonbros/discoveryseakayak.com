43135eec25b7b94ddf136b1921c55fef01e2501c
Steve Jamesson <steve@groovywebdesign.com>
2014-02-16 17:27:32 -0500

    footer content

--- wp/wp-content/themes/dsk/lib/scripts.php
+++ wp/wp-content/themes/dsk/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5e9019527a6f44029d88d8fb868479d5');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ae75b33e0369a21a303c3a516eb8b0ee');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- wp/wp-content/themes/dsk/templates/footer.php
+++ wp/wp-content/themes/dsk/templates/footer.php
@@ -1,8 +1,19 @@
 <footer class="content-info container" role="contentinfo">
   <div class="row">
-    <div class="col-lg-12">
-      <?php dynamic_sidebar('sidebar-footer'); ?>
-      <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+      <p class="copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+    </div>
+    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+      <div class="footer-socials">
+        #socials
+      </div>
+    </div>
+    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+      <p class="credit">Site by 
+        <a href="http://www.groovywebdesign.com/" title="We make great websites!" target="_blank">
+          <img src="http://wpassets.groovywebdesign.com/gwd-foot-logo-white.png" alt="Groovy Web Design">
+        </a>
+      </p>
     </div>
   </div>
 </footer>
