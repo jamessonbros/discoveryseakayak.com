cbdd2b30a896ff1fb4dde4f3219385bf65ed7538
Steve Jamesson <steve@groovywebdesign.com>
2014-02-21 17:57:06 -0500

    change guides alternative to right even, left odd

--- wp/wp-content/themes/dsk/template-guides.php
+++ wp/wp-content/themes/dsk/template-guides.php
@@ -15,7 +15,7 @@ $guides = get_field('guides');
     <?php foreach ($guides as $guide): ?>
     <!-- <pre><?php print_r($guide) ?></pre> -->
       <div class="row guide">
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 <?php echo $i%2 == 0 ? 'col-sm-push-6 col-md-push-6 col-lg-push-6' : '' ?>">
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 <?php echo $i%2 == 1 ? 'col-sm-push-6 col-md-push-6 col-lg-push-6' : '' ?>">
           <h2 class="guide-name"><?php echo $guide['name'] ?></h2>
           <?php if ($guide['byline']): ?>
             <p class="lead"><?php echo $guide['byline'] ?></p>
@@ -24,7 +24,7 @@ $guides = get_field('guides');
             <?php echo $guide['text'] ?>
           </div>
         </div>
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 <?php echo $i%2 == 0 ? 'col-sm-pull-6 col-md-pull-6 col-lg-pull-6' : '' ?>">
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 <?php echo $i%2 == 1 ? 'col-sm-pull-6 col-md-pull-6 col-lg-pull-6' : '' ?>">
           <img src="<?php echo $guide['image']['sizes']['large'] ?>" class="img-responsive" alt="<?php echo $guide['name'] ?>" title="<?php echo $guide['name'] ?>">
         </div>
       </div>
