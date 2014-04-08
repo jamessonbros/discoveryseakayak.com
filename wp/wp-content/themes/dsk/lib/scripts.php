a6dfdce78e5b1d4f3f553ed23d83baa2a5876133
Steve Jamesson <steve@groovywebdesign.com>
2014-04-08 18:23:13 -0400

    move carousel-indicators down a bit

--- wp/wp-content/themes/dsk/lib/scripts.php
+++ wp/wp-content/themes/dsk/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '08306d9999d624dcd0b68dbf87e1da5c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'bd34fb430c4135ba41d53127e0fa02ae');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
